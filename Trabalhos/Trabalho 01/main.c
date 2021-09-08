eab275bf3123cda9f7ce101b4c9e313628033977
Luiss1569 <rcsrcs1569@gmail.com>
2021-09-08 14:16:11 -0300

    Trabalho 02 - add Comentarios

--- Trabalhos/Trabalho 01/main.c
+++ Trabalhos/Trabalho 01/main.c
@@ -8,43 +8,71 @@
 //VocÃª deverÃ¡ modificar apenas a funÃ§Ã£o abaixo, vocÃª 
 //nÃ£o pode modificar a assinatura dela
 
+
+/*
+  Função resposavel por receber de fato a string e realizar a concatenação 
+  juntamente com as analizes de somatoria, onde retorna a soma de espaços da 
+  string 
+*/
 int analizaString(char* sin, char* sres, int* tam){
+  //Iniciando a variavel local espacos com 0
   int espacos = 0;
 
+  //loop que irá rodar do tamanho da string
   for(int i = 0; sin[i] != '\0'; i++){
 
-    if(sin[i] == '\n'){
-      continue;
+    //teste logico para verificar se o caracter é um caracter valido
 
-    }else if(sin[i] == ' '){
-      espacos = espacos + 1;
-    }
-
+    //caso for quebra de linha apenas pula para o proximo ciclo do loop
+    if(sin[i] == '\n') continue;
+    
+     //caso for espaço,somo 1 na variavel espacos
+    if(sin[i] == ' ') espacos++;
+     
+    //atribuo o caracter na variavel da concatenação, na posição adequada
     sres[*tam] = sin[i];
+    //somo 1 no tamanho total da string
     *tam = *tam + 1;
   }
 
+  // retorno o tamanho de espaços da string adiconada
   return espacos;
 
 }
 
+/*
+  Função responsavel por receber as variaveis e retornar a string cocatenada
+  com seus com a contagem de seus respectivos espaços e caracteres 
+*/
 int concatena(char* s1, char* s2, char* sres, int* espacos){
-  //===============
-  //IMPLEMENTE AQUI
-  //===============
 
+  // variavel resposavel pela somatoria dos caracteres inciando em 0
   int tamTotal = 0; 
-  *espacos = 0;
 
-  *espacos += analizaString(s1, sres, &tamTotal);
+  /*
+    Chamando a função para executar a concatenação da string e as somatorias
+    e atribuindo seu retorno a variavel espaços
+  */
+  *espacos = analizaString(s1, sres, &tamTotal);
 
+  /*
+    Adicionando um espaço entre as string que serão concatenadas e somo 1 no 
+    tamanho da string
+  */
   sres[tamTotal++] = ' ';
+  //somando mais um espaço
   *espacos = *espacos + 1;
 
+  /*
+    Chamando a função para executar a concatenação da string e as somatorias
+    e atribuindo o valor da variavel espacos mais o seu retorno
+  */
   *espacos += analizaString(s2, sres, &tamTotal);
 
+  //Adicionando o caracter nulo no final da string
   sres[tamTotal] = '\0';
   
+  //retornando o resultado da somatoria de caracteres validos da string
   return tamTotal;
 
 }
