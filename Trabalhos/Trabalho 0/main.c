e47f4a4067bdf31261928b0f6f508177e4d7b14b
Luiss1569 <rcsrcs1569@gmail.com>
2021-09-11 17:37:15 -0300

    update template base

---
+++ Trabalhos/Trabalho 0/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define true 1
+#define false 0
+typedef int bool;
+
+/*
+  Luis Ricardo Albano Santos - 2021031844
+  gcc -pedantic-errors -Wall main.c -o main.exe && ./main.exe 
+*/
+
+int main(void) {
+  
+  printf("Hello Word");
+  
+  return 0;
+
+}
