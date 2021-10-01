b717eeeaf0f73d9a95d4df57062d7b41ed6922d8
Luis Ricardo <luis.ricardo@tech4h.com.br>
2021-10-01 19:38:57 -0300

    exercicio 2 - deleta recursivo

--- Exercicios/Exercicio 02/main.c
+++ Exercicios/Exercicio 02/main.c
@@ -36,19 +36,42 @@ void apagaLista(p_no lista) {
 }
 
 void printaElemento(p_no elemento) {
-    if(elemento == NULL) return;
-    printf(" %d ", elemento->dado);
+    if (elemento == NULL) return;
+    printf("%d ", elemento->dado);
 }
 
 void printaLista(p_no lista) {
     if (lista == NULL) return;
-    printaLista(lista->prox);
     printaElemento(lista);
+    printaLista(lista->prox);
+
+}
+
+p_no deletaElemento(p_no lista, int dado) {
+    if (lista == NULL || lista->prox == NULL) return NULL;
+
+    if ((lista->prox)->dado == dado) {
+        p_no aux = lista->prox;
+        lista->prox = lista->prox->prox;
+
+        free(aux);
+        return lista;
+    }
+    else if (lista->dado == dado) {
+        p_no aux = lista->prox;
+        free(lista);
+        return aux;
+    }
+    
+    deletaElemento(lista->prox, dado);
+
+    return NULL;
+
 }
 
 p_no buscaElemento(p_no lista, int dado) {
     if (lista == NULL) return NULL;
-    if (lista->dado == dado) return lista; 
+    if (lista->dado == dado) return lista;
 
     p_no no = buscaElemento(lista->prox, dado);
 
@@ -59,18 +82,22 @@ int main(void) {
 
     p_no lista = criaLista();
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 10; i++) {
         lista = adicionaLista(lista, i);
     }
 
     printaLista(lista);
-
+    printf("\n");
     p_no elemento = buscaElemento(lista, 0);
     printaElemento(elemento);
-
+    printf("\n");
     elemento = buscaElemento(lista, 4);
     printaElemento(elemento);
+    printf("\n");
+    lista = deletaElemento(lista, 9);
 
+    printaLista(lista);
+    printf("\n");
     apagaLista(lista);
 
     printf("\n");
