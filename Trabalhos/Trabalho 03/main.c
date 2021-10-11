a1cd141f7e736868d6af21bc0aac47f4ecc701fa
Luis Ricardo <luis.ricardo@tech4h.com.br>
2021-10-11 19:02:53 -0300

    add comentarios

--- Trabalhos/Trabalho 03/main.c
+++ Trabalhos/Trabalho 03/main.c
@@ -47,57 +47,75 @@ void imprime_macacos(p_Macaco M, int num_macacos) {
   }
 }
 
+// Função responsavel por iniciar os macacos
 p_Macaco inicializa_macacos(int n_macacos) {
+  //Aloco um vetor com o tamanho de macacos necessarios
   p_Macaco macacos = malloc(n_macacos * sizeof(Macaco));
 
+  //Faço a inicialização dos dados de cada macaco
   for (int i = 0; i < n_macacos; i++) {
     macacos[i].altura = 0;
     macacos[i].lista_blocos = NULL;
   }
-
+  //Retorno os macacos
   return macacos;
 }
 
+//Função resposavel por criar um bloco
 p_Bloco add_bloco(p_Bloco lista_blocos, int id, int altura) {
+  //Aloco um bloco
   p_Bloco bloco = malloc(sizeof(Bloco));
 
+  //atribuo os valores necessarios
   bloco->id = id;
   bloco->altura = altura;
   bloco->prox = lista_blocos;
 
+  //retorno o bloco criado
   return bloco;
 }
 
+// Função responsavel por add um bloco no macoco que possui a menor altura e indice
 void bota_no_menor(p_Macaco macacos, int n_macacos, int id, int altura) {
   int menor = 0;
 
+  //Verifico se existe mais de um macaco
   if (n_macacos > 2) {
+    //Percorro o vetor de macacos para encontrar o  de menor altura
     for (int i = 1; i < n_macacos; i++) {
+      //Verifico o macaco com menor altura e indice pelo > sem o =
       if (macacos[menor].altura > macacos[i].altura) {
+
+        //Atribuo o indice do menor
         menor = i;
       }
     }
   }
 
+  //Pego o macaco com menor altura 
   p_Macaco macaco = &macacos[menor];
 
+  //Aloco um bloco e atribuo a esse macaco somando sua altura
   p_Bloco bloco = add_bloco(macaco->lista_blocos, id, altura);
   macaco->altura = macaco->altura + altura;
   macaco->lista_blocos = bloco;
 
 }
 
+// Função responsavel por liberar os blocos
 void libera_blocos(p_Bloco blocos) {
   if (blocos == NULL) return;
   libera_blocos(blocos->prox);
   free(blocos);
 }
 
+// Função responsavel por liberar os macacos
 void libera_macacos(p_Macaco macacos, int n_macacos) {
-  for (int i = 1; i < n_macacos; i++) {
+  //Percorro o vetor de macacos liberando seus blocos 
+  for (int i = 0; i < n_macacos; i++) {
     libera_blocos(macacos[i].lista_blocos);
   }
-
+  //libero o array de macacos
   free(macacos);
 }
 
